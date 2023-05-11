111d7ce84dd7a0537654f9e702e34d1b4931637a
timofeyevdomainorg <timofeyevdomainorg@gmail.com>
2023-05-11 16:42:10 +0200

    hasc2

--- custom_components/telegram_reader/config_flow.py
+++ custom_components/telegram_reader/config_flow.py
@@ -1,22 +1,27 @@
-from homeassistant import config_entries
+from homeassistant import config_entries, exceptions
 from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 import voluptuous as vol
-DOMAIN = "telegram_reader"
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
 
 class MyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
-    def __init__(self):
-        self._user_input = None
-
     async def async_step_user(self, user_input=None) -> FlowResult:
         errors = {}
         if user_input is not None:
-            # В этом месте вы можете добавить код для авторизации в Telegram
-            self._user_input = user_input  # Сохраняем данные от пользователя
-            return await self.async_step_channels()
+            self.api_id = user_input["api_id"]
+            self.api_hash = user_input["api_hash"]
+            self.phone = user_input["phone"]
+
+            self.client = TelegramClient('anon', self.api_id, self.api_hash)
+            await self.client.connect()
+
+            if not await self.client.is_user_authorized():
+                await self.client.send_code_request(self.phone)
+                return await self.async_step_verify()
 
         return self.async_show_form(
             step_id="user",
@@ -30,18 +35,53 @@ class MyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             errors=errors,
         )
 
+    async def async_step_verify(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            try:
+                await self.client.sign_in(self.phone, user_input["code"])
+            except SessionPasswordNeededError:
+                return self.async_show_form(
+                    step_id="password",
+                    data_schema=vol.Schema({vol.Required("password"): str}),
+                    errors=errors,
+                )
+
+            return await self.async_step_channels()
+
+        return self.async_show_form(
+            step_id="verify",
+            data_schema=vol.Schema({vol.Required("code"): str}),
+            errors=errors,
+        )
+
+    async def async_step_password(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            await self.client.sign_in(password=user_input["password"])
+            return await self.async_step_channels()
+
+        return self.async_show_form(
+            step_id="password",
+            data_schema=vol.Schema({vol.Required("password"): str}),
+            errors=errors,
+        )
+
     async def async_step_channels(self, user_input=None) -> FlowResult:
         errors = {}
         if user_input is not None:
-            self._user_input.update(user_input)  # Объединяем данные от пользователя с новыми данными
-            return self.async_create_entry(title="Telegram Channels", data=self._user_input)
+            return self.async_create_entry(title="Telegram Channels", data=user_input)
+
+        dialogs = await self.client.get_dialogs()
+        channels = {dialog.name: dialog.name for dialog in dialogs if dialog.is_channel}
 
         return self.async_show_form(
             step_id="channels",
             data_schema=vol.Schema(
                 {
-                    vol.Required("channels"): str,
+                    vol.Required("channels"): vol.MultiSelect(channels),
                 }
             ),
             errors=errors,
         )
+
