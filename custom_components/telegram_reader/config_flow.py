3a3e9b1660c09d9a2f926035ff00a1d85d1b208e
timofeyevdomainorg <timofeyevdomainorg@gmail.com>
2023-05-11 15:35:42 +0200

    hasc2

--- custom_components/telegram_reader/config_flow.py
+++ custom_components/telegram_reader/config_flow.py
@@ -8,11 +8,15 @@ class MyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
+    def __init__(self):
+        self._user_input = None
+
     async def async_step_user(self, user_input=None) -> FlowResult:
         errors = {}
         if user_input is not None:
             # В этом месте вы можете добавить код для авторизации в Telegram
-            return await self.async_step_channels(user_input)
+            self._user_input = user_input  # Сохраняем данные от пользователя
+            return await self.async_step_channels()
 
         return self.async_show_form(
             step_id="user",
@@ -29,7 +33,8 @@ class MyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_channels(self, user_input=None) -> FlowResult:
         errors = {}
         if user_input is not None:
-            return self.async_create_entry(title="Telegram Channels", data=user_input)
+            self._user_input.update(user_input)  # Объединяем данные от пользователя с новыми данными
+            return self.async_create_entry(title="Telegram Channels", data=self._user_input)
 
         return self.async_show_form(
             step_id="channels",
