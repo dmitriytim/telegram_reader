863930a22361ddd3a9d6b06b67d1db2ada973130
timofeyevdomainorg <timofeyevdomainorg@gmail.com>
2023-05-11 15:25:28 +0200

    hasc2

--- custom_components/telegram_reader/__init__.py
+++ custom_components/telegram_reader/__init__.py
@@ -4,23 +4,14 @@ from .telegram_reader import TelegramReader  # Убедитесь, что Telegr
 from homeassistant import config_entries
 from .config_flow import MyConfigFlow
 
+
 DOMAIN = "telegram_reader"
 
 async def async_setup(hass, config):
-    hass.config_entries.async_register_flow_handler(DOMAIN, MyConfigFlow)
+    hass.data[DOMAIN] = {}
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    # Создаем экземпляр нашего компонента с данными из настроек пользователя
-    telegram_reader = TelegramReader(entry.data)
-
-    # Устанавливаем экземпляр компонента для использования в других частях нашей интеграции
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = telegram_reader
-
-    # Добавляем сенсоры для нашего компонента
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
-
-    # Возвращает True, если инициализация прошла успешно
+    hass.data[DOMAIN][entry.entry_id] = TelegramReader(entry.data)
     return True
+
