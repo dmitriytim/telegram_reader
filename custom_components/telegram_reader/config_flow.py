f36292c07dd4c651cc22d9fafb13662035f01eac
timofeyevdomainorg <timofeyevdomainorg@gmail.com>
2023-05-12 18:41:41 +0200

    old chanels

--- custom_components/telegram_reader/config_flow.py
+++ custom_components/telegram_reader/config_flow.py
@@ -113,8 +113,7 @@ class MyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             )
 
         dialogs = await self.client.get_dialogs()
-        channels = {dialog.entity.id: dialog.entity.title for dialog in dialogs if
-                    isinstance(dialog.entity, types.Channel)}
+        channels = {dialog.entity.id: dialog.entity.title for dialog in dialogs if isinstance(dialog.entity, types.Channel)}
 
         return self.async_show_form(
             step_id="channels",
@@ -127,6 +126,7 @@ class MyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             ),
             errors=errors,
         )
+
 
 
 
